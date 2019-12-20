0837a3bba872081b484f242937a15d9d3d35f0c5
Jordan.Presnell <jordan.presnell@KC-L-C02XTB8JJG5J.local>
2019-12-20 10:51:45 -0600

    firebase data save function

--- src/app/public/register/register.page.ts
+++ src/app/public/register/register.page.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/auth';
-import { auth } from 'firebase/app';
-
+import { auth, database } from 'firebase/app';
+import { Contacts, Contact, ContactField, ContactName } from '@ionic-native/contacts/ngx';
+import { Router } from '@angular/router';
+import { AngularFirestore, AngularFirestoreDocument } from '@angular/fire/firestore';
 
 @Component({
   selector: 'app-register',
@@ -10,7 +12,7 @@ import { auth } from 'firebase/app';
 })
 export class RegisterPage implements OnInit {
 
-  constructor(public afAuth: AngularFireAuth) { }
+  constructor(public afAuth: AngularFireAuth, private router: Router, private contacts: Contacts, private fStore: AngularFirestoreDocument) { }
 
   username: string = ""
   password: string = ""
@@ -22,17 +24,32 @@ export class RegisterPage implements OnInit {
   }
 
   async register() {
-    //verify that passwords match
+
+    // on the login() we are invoking the cordova native contact managmenet plugin
+    //let contact: Contact = this.contacts.create();
+
+    //contact.name = new ContactName(null, '', '');
+   // contact.phoneNumbers = [new ContactField('mobile', '')];
+    // we need to figure out where we want to save these contacts
+    // we can only test this nativly
+
+   // contact.save() {
+    //  firebase.database().ref(associatedLead).set({
+       //   contact: document.getElementById(contact.name).nodeValue,
+        //  contactPhoneNumber: document.getElementById(contact.phoneNumbers).nodeValue
+    //  });
+  //  }
+    // verify that passwords match
     const { username, password, cpassword } = this
     if (password !== cpassword) {
-      return console.error("yo, your shit don't match")
+      return console.error('yo, your shit dont match');
     }
 
 
     try {
-      //use firebase to create username and password
+      // use firebase to create username and password
       const res = await this.afAuth.auth.createUserWithEmailAndPassword(username + '@gmail.com', password)
-      console.log(res)
+      console.log(res);
     } catch (error) {
       console.dir(error);
     }
